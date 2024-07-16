7fd2d89b965161b1f9749d58f919a2522fecb76b
kevening <44373433+kevening@users.noreply.github.com>
2024-07-16 10:05:54 +0200

    MongoDB repo version changes in agentNode and opsManager node configuration

--- AWS_OpsManager/agentNode_user_data.sh
+++ AWS_OpsManager/agentNode_user_data.sh
@@ -1,7 +1,7 @@
 #!/bin/sh -xe
-
-curl -OL ${mmsBaseUrl}/download/agent/automation/mongodb-mms-automation-agent-manager-11.0.6.6981-1.x86_64.rhel7.rpm
-sudo rpm -U mongodb-mms-automation-agent-manager-11.0.6.6981-1.x86_64.rhel7.rpm
+sudo yum update -y
+curl -OL ${mmsBaseUrl}/download/agent/automation/mongodb-mms-automation-agent-manager-latest.x86_64.rpm
+sudo rpm -U mongodb-mms-automation-agent-manager-latest.x86_64.rpm
 sudo rm /etc/mongodb-mms/automation-agent.config
 sudo tee /etc/mongodb-mms/automation-agent.config <<EOF
 mmsGroupId=${mmsGroupId}
@@ -14,7 +14,12 @@ maxLogFiles=10
 maxLogFileSize=268435456
 EOF
 sudo yum -y install cyrus-sasl cyrus-sasl-gssapi cyrus-sasl-plain krb5-libs libcurl net-snmp net-snmp-libs openldap openssl xz-libs
+sudo yum install -y openldap openldap-clients openldap-servers
 sudo mkdir -p /data
 sudo chown mongod:mongod /data
 sudo systemctl start mongodb-mms-automation-agent.service
 sudo systemctl enable mongodb-mms-automation-agent.service
+echo "preserve_hostname: true" | sudo tee -a /etc/cloud/cloud.cfg 
+export pubdns=$(curl http://169.254.169.254/latest/meta-data/public-hostname)
+sudo hostnamectl set-hostname $pubdns
+sudo reboot
